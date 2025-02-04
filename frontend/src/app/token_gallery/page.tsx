a975cb263869045e0956e11e97a242feb7e39c6b
Antismart <timbwamoses83@gmail.com>
2025-02-04 18:20:11 +0300

    made changes

--- frontend/src/app/token_gallery/page.tsx
+++ frontend/src/app/token_gallery/page.tsx
@@ -58,7 +58,6 @@ export default function TokenGallery() {
     args: totalSupply
       ? [BigInt(totalSupply.toString()) - BigInt(1)]
       : undefined,
-    enabled: !!totalSupply,
   });
 
   useEffect(() => {
